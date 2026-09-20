988ae69762da9f20be0d455ba816f381b9d0e2f6
MikhailichenkoArtemy <Tyuiu.MikhailichenkoAI@outlook.com>
2026-09-20 15:01:18 +0500

    Task2

--- Tyuiu.MikhailichenkoAI.Sprint2.Task2.V24.Lib/DataService.cs
+++ Tyuiu.MikhailichenkoAI.Sprint2.Task2.V24.Lib/DataService.cs
@@ -1,8 +1,27 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint2;
+﻿
+using tyuiu.cources.programming.interfaces.Sprint2;
+
 namespace Tyuiu.MikhailichenkoAI.Sprint2.Task2.V24.Lib
 {
     public class DataService : ISprint2Task2V24
     {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            bool res = false;
 
+            if ((x >= 3 && x <= 5 && y >= 3 && y <= 7) ||    
+                (x >= 9 && x <= 12 && y >= 3 && y <= 5) ||   
+                (x >= 6 && x <= 7 && y >= 5 && y <= 8) ||    
+                (x >= 8 && x <= 13 && y == 6) ||             
+                (x >= 6 && x <= 12 && y >= 9 && y <= 10) ||  
+                (x >= 3 && x <= 6 && y == 11) ||             
+                (x >= 11 && x <= 12 && y >= 11 && y <= 12) || 
+                (x == 7 && y == 12))                         
+            {
+                res = true; 
+            }
+
+            return res;
+        }
     }
 }
--- Tyuiu.MikhailichenkoAI.Sprint2.Task2.V24/Program.cs
+++ Tyuiu.MikhailichenkoAI.Sprint2.Task2.V24/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace Tyuiu.MikhailichenkoAI.Sprint2.Task2.V24
+﻿using Tyuiu.MikhailichenkoAI.Sprint2.Task2.V24.Lib;
+
+namespace Tyuiu.MikhailichenkoAI.Sprint2.Task2.V24
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Михайличенко А. И. | ИИПБ26-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор if – полная и короткая форма записи                      *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Выполнил: Михайличенко А. И.                                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает целые значения с клавиатуры и   *");
+            Console.WriteLine("* вычисляет, находится ли точка с координатами X,Y в заштрихованной       *");
+            Console.WriteLine("* области.                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите координату X: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите координату Y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            if (res)
+            {
+                Console.WriteLine("Точка находится В заштрихованной области.");
+            }
+            else
+            {
+                Console.WriteLine("Точка находится ВНЕ заштрихованной области.");
+            }
+
+            Console.ReadKey();
         }
     }
 }
